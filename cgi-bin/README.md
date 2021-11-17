6f41e9596a7f1b22523e246127ce6720e184a2a2
Andrew.Aham <ahamchukwuemeka2@gmail.com>
2021-11-17 16:27:57 +0100

    Updated all files

---
+++ cgi-bin/README.md
@@ -0,0 +1,14 @@
+### Athlete Timing Data App
+
+# Hello :wave:
+
+This is a python cgi programming mini project
+
+## Available Scripts
+
+In the project directory, run:
+
+### `python simplehttpd.py`
+
+Runs the app in the development mode.\
+Open [http://localhost:8080](http://localhost:8080) to view it in the browser.
---
+++ cgi-bin/get_name_from_store.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+db_name = 'coachdata.sqlite'
+
+def get_names_from_store():
+    connection = sqlite3.connect(db_name)
+    cursor = connection.cursor()
+    results = cursor.execute("""SELECT name FROM athletes""")
+    response = [row[0] for row in results.fetchall()]
+    connection.close()
+    return(response)
+
+def get_namesID_from_store():
+    connection = sqlite3.connect(db_name)
+    cursor = connection.cursor()
+    results = cursor.execute("""SELECT name, id FROM athletes""")
+    response = results.fetchall()
+    connection.close()
+    return(response)
+
